6c2c8531577abc3bbc89eb62caf4fd68e68830ba
ryleytraverse <ryleytraverse2@gmail.com>
2024-06-30 14:17:50 -0600

    Fixing Plot Titles and Adding MAE

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import helper_functions as hf
+import utils as hf
 from scipy.signal import savgol_filter
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -16,6 +16,7 @@ def main():
     df_bowling['scratch'] = df_bowling['Game1'] + df_bowling['Game2'] + df_bowling['Game3']
     df_bowling['average'] = df_bowling['scratch'].astype(float)/3
 
+    # Create dashboard sidebar with page infromation
     with st.sidebar:
         st.header('This streamlit dashboard was made for my personal use and allows for updates, tracking, and prediction my bowling performance.')
         st.markdown("Link to code in Github: [streamlit_dashboard_code](%s)" % "https://github.com/ryleytraverse/streamlit_bowling_app/blob/main/main.py")
@@ -24,12 +25,14 @@ def main():
         st.subheader('The third tab shows my predicted bowling score for the following week.')
         st.subheader('The fourth tab allows for me to update the data with my scores after I bowl.')
 
+    # Create the all time analysis tab
     with overall_tab:
         title_container = st.container()
         with title_container:
             st.title('Ryley Traverse Bowling League Analysis Interactive Dashboard')
         hf.create_analysis_tab(df_bowling, 'All Time Game Average')
 
+    # Create league by league analysis tab
     with league_tab:
         title_container = st.container()
         with title_container:
@@ -40,6 +43,7 @@ def main():
         df_league = df_bowling[df_bowling['League']==league]
         hf.create_analysis_tab(df_league, 'Selected League Game Average')
 
+    # Create tab with next weeks prediction
     with prediction_tab:
         title_container = st.container()
         smooth_plot = st.container()
@@ -49,42 +53,52 @@ def main():
             st.title('Ryley Traverse Bowling League Analysis Interactive Dashboard')
 
         with smooth_plot:
+            # Calculate all time week number
             df_bowling_smooth = hf.calculate_all_time_weeks(df_bowling)
-            # Smooth scratch recordings
+            # Smooth scratch recordings and plot
             window_size = st.slider("Choose Smoothing Window Size", 3, 30, 7)
             poly_order = st.number_input("Choose Smoothing Polynomial Order", min_value=1, max_value=5, value=2)
-            df_bowling_smooth['scratch_smoothed'] = savgol_filter(df_bowling_smooth['scratch'].to_numpy(), window_size, poly_order)
+            df_bowling_smooth['scratch_average_smoothed'] = savgol_filter(df_bowling_smooth['average'].to_numpy(), window_size, poly_order)
             fig_go = go.Figure()
-            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['scratch'], mode='lines+markers', name='Scratch', line=dict(color='magenta')))
-            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['scratch_smoothed'], mode='lines+markers', name='Scratch Smoothed', line=dict(color='indigo')))
-            t = 'Plot of Scratch over Time after Smoothing'
+            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['average'], mode='lines+markers', name='Scratch', line=dict(color='magenta')))
+            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['scratch_average_smoothed'], mode='lines+markers', name='Scratch Smoothed', line=dict(color='indigo')))
+            t = 'Plot of Scratch Bowling Average over Time after Smoothing'
             fig_go.update_layout(title=t, xaxis_title='All Time Week Number', yaxis_title='Score')
             st.plotly_chart(fig_go)
 
+        # Fit regression model based on polynomial input and show accuracy and prediction
         with prediction_container:
+            # Fit Regression Model
             X = df_bowling_smooth['week_num_all_time'].to_numpy().reshape(-1, 1)
-            y = df_bowling_smooth['scratch'].to_numpy()
+            y = df_bowling_smooth['average'].to_numpy()
             model_poly_order = st.number_input("Choose Regression Model Polynomial Order", min_value=1, max_value=10, value=1)
             poly_reg = PolynomialFeatures(degree=model_poly_order)
             X_poly = poly_reg.fit_transform(X)
             lin_reg = LinearRegression()
             lin_reg.fit(X_poly, y)
+            # Get next time step and all past week's predictions
             next_time_step = max(df_bowling_smooth['week_num_all_time']) + 1
             X_pred = np.array(next_time_step).reshape(1, -1)
             X_pred_poly = poly_reg.transform(X_pred)
             pred = lin_reg.predict(X_pred_poly)
             preds = lin_reg.predict(X_poly)
-            t = 'Scratch Prediction for Next Week of Bowling: ' + str(round(pred[0]))
-            st.write(t)
+            # Calculate MAE and print out next week's prediction and past week's accuracy
+            mae = np.mean(abs(preds-y))
+            t = 'Scratch Bowling Average Prediction for Next Week of Bowling: ' + str(round(pred[0]))
+            st.subheader(t)
+            t = 'Past Week\'s Scratch Bowling Average Predictions Mean Absolute Error: ' + str(round(mae, 2))
+            st.subheader(t)
 
+            # Plot Data, Regression Line, and Next Weeks Prediction
             fig_go = go.Figure()
-            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['scratch'], mode='lines+markers', name='Scratch', line=dict(color='magenta')))
+            fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=df_bowling_smooth['average'], mode='lines+markers', name='Average Scratch', line=dict(color='magenta')))
             fig_go.add_trace(go.Scatter(x=df_bowling_smooth['week_num_all_time'], y=preds, mode='lines', name='Fitted Regression Line', line=dict(color='indigo')))
-            fig_go.add_trace(go.Scatter(x=[next_time_step], y=pred, mode='markers', name='Next League Scratch Prediction', line=dict(color='indigo')))
-            t = 'Plot of Scratch with Regression Line and Prediction Shown'
+            fig_go.add_trace(go.Scatter(x=[next_time_step], y=pred, mode='markers', name='Next League Scratch Bowling Average Prediction', line=dict(color='indigo')))
+            t = 'Plot of Scratch Bowling Average with Regression Line and Prediction Shown'
             fig_go.update_layout(title=t, xaxis_title='All Time Week Number', yaxis_title='Score')
             st.plotly_chart(fig_go)
 
+        # Create tab where bowling score csv can be updated with most current bowling performance
         with add_data_tab:
             title_container = st.container()
             with title_container:
@@ -101,6 +115,7 @@ def main():
                 df_saved.loc[len(df_saved)] = [league_name, league_date, int(league_week), int(game1), int(game2), int(game3)]
                 df_saved.to_csv('Bowling.csv')
 
+    # Optional Plotting Method:
     #     with plot_container_plt:
     #         fig_plt, ax = plt.subplots()
     #         ax.scatter(idk, idk2, s=10, color='cyan', label='Data')
---
+++ utils.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+    
+# Create analysis tabs for all time and league by league performance breakdowns
+def create_analysis_tab(df:pd.DataFrame, title_name:str):
+    scratch_over_time = st.container()
+    games_over_time = st.container()
+    game_average = st.container()
+
+    # Plot Average Bowling Scratch Over Time
+    with scratch_over_time:
+        fig_go = go.Figure()
+        fig_go.add_trace(go.Scatter(x=df['Date'], y=df['average'], mode='lines+markers', name='Average Scratch Over Time', line=dict(color='magenta')))
+        t = 'Plot of Scratch Bowling Average over Time'
+        fig_go.update_layout(title=t, xaxis_title='Date', yaxis_title='Average Scratch')
+        st.plotly_chart(fig_go)
+
+    # Plot of Scratch Performance in Each Game over Time
+    with games_over_time:
+        fig_go = go.Figure()
+        fig_go.add_trace(go.Scatter(x=df['Date'], y=df['Game1'], mode='lines+markers', name='Game 1 Score', line=dict(color='blue')))
+        fig_go.add_trace(go.Scatter(x=df['Date'], y=df['Game2'], mode='lines+markers', name='Game 2 Score', line=dict(color='cyan')))
+        fig_go.add_trace(go.Scatter(x=df['Date'], y=df['Game3'], mode='lines+markers', name='Game 3 Score', line=dict(color='lime')))
+        t = 'Plot of Bowling Score over Time for Each Game'
+        fig_go.update_layout(title=t, xaxis_title='Date', yaxis_title='Score')
+        st.plotly_chart(fig_go)
+
+    # Plot of Game Averages
+    with game_average:
+        x_values = ['Game 1', 'Game 2', 'Game 3']
+        y_values = []
+        y_values.append(np.mean(df['Game1']))
+        y_values.append(np.mean(df['Game2']))
+        y_values.append(np.mean(df['Game3']))
+        fig_go = go.Figure()
+        fig_go.add_trace(go.Bar(x=x_values, y=y_values, marker=dict(color= ['blue', 'cyan', 'lime'])))
+        fig_go.update_layout(title=title_name, xaxis_title='Game', yaxis_title='Average Score')
+        st.plotly_chart(fig_go)
+
+def calculate_all_time_weeks(df):
+    # Calculate all time week number
+    week_nums = []
+    count = 0
+    for l in df.League.unique():
+        df_temp = df[df['League']==l]
+        max_week = int(df_temp['Week'].iloc[-1])
+        weeks = list(df_temp['Week'])
+        weeks = [x+count for x in weeks]
+        count += max_week
+        week_nums = week_nums + weeks
+    df['week_num_all_time'] = week_nums
+    return df       
