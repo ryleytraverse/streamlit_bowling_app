db9327013b18cf804e815030af6caf33860ac675
ryleytraverse <ryleytraverse2@gmail.com>
2024-06-29 19:11:41 -0600

    Fixing requirements

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
     
 def create_analysis_tab(df:pd.DataFrame, title_name:str):
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import helper_functions as hf
 from scipy.signal import savgol_filter
